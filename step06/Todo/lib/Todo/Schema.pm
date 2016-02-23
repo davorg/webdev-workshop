ea3e37ba3018040944fbf14d4eb68c47a3a10527
Dave Cross <dave@dave.org.uk>
2016-02-23 12:30:43 +0000

    Store the items in a Javascrip array

---
+++ step06/Todo/lib/Todo/Schema.pm
@@ -0,0 +1,20 @@
+use utf8;
+package Todo::Schema;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use Moose;
+use MooseX::MarkAsMethods autoclean => 1;
+extends 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.07045 @ 2016-02-20 15:34:03
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:vVE+p7R6DZlode+faUm3PA
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
+1;
---
+++ step06/db/todo.sql
@@ -0,0 +1,8 @@
+CREATE TABLE item (
+  id integer not null auto_increment primary key,
+  title varchar(200) not null,
+  description text,
+  due datetime,
+  done boolean not null default false
+) Engine=InnoDB;
+
